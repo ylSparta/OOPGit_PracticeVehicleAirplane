b303ea7b78ae4d47d8f4de8e5515e27b09efe1ae
YangaL96 <ylomuro12@gmail.com>
2021-01-01 18:02:31 +0000

    Airplane class exercise is complete

--- VehicleAirplaneInheritance/VehicleAirplaneInheritance/Airplane.cs
+++ VehicleAirplaneInheritance/VehicleAirplaneInheritance/Airplane.cs
@@ -4,8 +4,50 @@ using System.Text;
 
 namespace VehicleAirplaneInheritance
 {
-    public class Airplane
+    public class Airplane: Vehicle
     {
+        private string _airline;
+        private int _altitude;
 
+        public Airplane() { }
+
+        public Airplane(int capacity)
+        {
+            _capacity = capacity;
+        }
+
+        public Airplane(int capacity, int speed, string airline) 
+        {
+            _capacity = capacity;
+            _speed = speed;
+            _airline = airline;
+        }
+
+        public void Ascend(int distance)
+        {
+            _altitude += distance;
+        }
+
+        public void Descend(int distance)
+        {
+            _altitude -= distance;
+        }
+
+        public new string Move()
+        {
+            Position += _speed;
+            return $"Moving along at an altitude of {_altitude} metres";
+        }
+
+        public new string Move(int times)
+        {
+            Position += times*_speed;
+            return $"Moving along {times} times at an altitude of {_altitude} metres";
+        }
+
+        public override string ToString()
+        {
+            return $"Thank you for flying {_airline}: {base.ToString()}: capacity: {_capacity}, passengers: {NumPassengers}, speed: {_speed}, position: {Position}, altitude: {_altitude} ";
+        }
     }
 }
--- VehicleAirplaneInheritance/VehicleAirplaneInheritance/Vehicle.cs
+++ VehicleAirplaneInheritance/VehicleAirplaneInheritance/Vehicle.cs
@@ -4,9 +4,44 @@ namespace VehicleAirplaneInheritance
 {
     public class Vehicle
     {
+        protected int _capacity;
+        protected int _numPassengers;
+        protected int _speed = 10;
+
+        public int NumPassengers { get; set; }
+        public int Position { get; set; }
+
+        public Vehicle() { }
+
+        public Vehicle(int capacity, int speed = 10) 
+        {
+            _capacity = capacity;
+            _speed = speed;
+        }
+
+
+        public string Move()
+        {
+            return $"Moving along";
+        }
+
+        public string Move(int times)
+        {
+            return $"Moving along {times} times";
+        }
+
+
         static void Main(string[] args)
         {
-
+            Airplane a = new Airplane(200, 100, "JetsRUs")
+            { NumPassengers = 150 };
+            a.Ascend(500);
+            Console.WriteLine(a.Move(3));
+            Console.WriteLine(a);
+            a.Descend(200);
+            Console.WriteLine(a.Move());
+            a.Move();
+            Console.WriteLine(a);
         }
     }
 }
