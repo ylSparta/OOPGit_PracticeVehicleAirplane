5659bbb3775ab686bcefa4388e2e5fe7193174c1
YangaL96 <ylomuro12@gmail.com>
2021-01-01 11:36:55 +0000

    intial vs project has been made

---
+++ VehicleAirplaneInheritance/VehicleAirplaneInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleAirplaneInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
